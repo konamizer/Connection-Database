b289486cdaec8d80ef9f206a846aabf819ca456c
konamizer <60470373+konamizer@users.noreply.github.com>
2022-09-20 15:44:01 +0700

    Add files via upload

---
+++ latihan_database_connect/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIBKMNet.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public int Password { get; set; }
+       
+    }
+}
---
+++ latihan_database_connect/Program.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Data.SqlClient;
+using SIBKMNet.Models;
+namespace latihan_database_connect
+{
+    class Program
+    {
+        SqlConnection sqlConnection;
+
+        /*
+         * Data Source -> Server
+         * Initial Catalog -> Database
+         * User ID -> username
+         * Password -> password
+         * Connect Timeout
+         */
+        string connectionString = "Data Source=LAPTOP-LDJ6J02G;Initial Catalog=Db_Kantor;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            program.GetById(1);
+
+            Country country = new Country()
+            {
+                Id = 05,
+                Username = "admin1",
+                Password = 123456,
+
+            };
+            program.Insert(country);
+            program.Update(country);
+            //program.Delete(country);
+
+            program.GetAll();
+        }
+
+        void GetAll()
+        {
+            string query = "SELECT * FROM Db_Login";
+
+            sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            try
+            {
+                sqlConnection.Open();
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine(sqlDataReader[0] + " - " + sqlDataReader[1] + " - " + sqlDataReader[2]);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Rows");
+                    }
+                    sqlDataReader.Close();
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+
+        void GetById(int id)
+        {
+            string query = "SELECT * FROM Login WHERE Id = @Id";
+
+            SqlParameter sqlParameter = new SqlParameter();
+            sqlParameter.ParameterName = "@Id";
+            sqlParameter.Value = id;
+
+            sqlConnection = new SqlConnection(connectionString);
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.Parameters.Add(sqlParameter);
+            try
+            {
+                sqlConnection.Open();
+                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+                {
+                    if (sqlDataReader.HasRows)
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine(sqlDataReader[0] + " - " + sqlDataReader[1] + " - " + sqlDataReader[2]);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Data Rows");
+                    }
+                    sqlDataReader.Close();
+                }
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+
+        void Insert(Country country)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@Id";
+                sqlParameter.Value = country.Id;
+                sqlParameter.ParameterName = "@Username";
+                sqlParameter.Value = country.Username;
+                sqlParameter.ParameterName = "@Password";
+                sqlParameter.Value = country.Password;
+
+                sqlCommand.Parameters.Add(sqlParameter);
+
+                try
+                {
+                    sqlCommand.CommandText = "INSERT INTO Db_Login " + "(Id) VALUES (@Id)" +
+                        "(Username) VALUES (@Username)" + "(Password) VALUES (@Password)";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException);
+                }
+            }
+        } 
+
+        // Update
+        void Update(Country country)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@Id";
+                sqlParameter.Value = country.Id;
+                sqlParameter.ParameterName = "@Username";
+                sqlParameter.Value = country.Username;
+                sqlParameter.ParameterName = "@Password";
+                sqlParameter.Value = country.Password;
+
+                sqlCommand.Parameters.Add(sqlParameter);
+
+                try
+                {
+                    sqlCommand.CommandText = "UPDATE INTO Db_Login " + "(Id) VALUES (@Id)" +
+                        "(Username) VALUES (@Username)" + "(Password) VALUES (@Password)";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException);
+                }
+            }
+        }
+        // Delete
+        void Delete(Country country)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.Transaction = sqlTransaction;
+
+                SqlParameter sqlParameter = new SqlParameter();
+                sqlParameter.ParameterName = "@Id";
+                sqlParameter.Value = country.Id;
+                sqlParameter.ParameterName = "@Username";
+                sqlParameter.Value = country.Username;
+                sqlParameter.ParameterName = "@Password";
+                sqlParameter.Value = country.Password;
+
+                sqlCommand.Parameters.Add(sqlParameter);
+
+                try
+                {
+                    sqlCommand.CommandText = "DROP INTO Db_Login " + "(Id) VALUES (@Id)" +
+                        "(Username) VALUES (@Username)" + "(Password) VALUES (@Password)";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException);
+                }
+            }
+        }
+    }
+}
